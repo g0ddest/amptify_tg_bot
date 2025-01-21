623e4d4d2af54dfe7c4efdcbd9eaaba77d6ba6b5
Vitaliy Velikodniy <vitaliy@velikodniy.name>
2025-01-21 09:02:44 +0200

    get config via env

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -11,18 +11,36 @@ type Config struct {
 
 func CreateConfig() (*Config, error) {
 	config := &Config{}
+
 	file, err := os.Open("./configs/config.yaml")
-	if err != nil {
+	if err == nil {
+		defer func(file *os.File) {
+			_ = file.Close()
+		}(file)
+
+		d := yaml.NewDecoder(file)
+		if err := d.Decode(&config); err != nil {
+			return nil, err
+		}
+	} else if !os.IsNotExist(err) {
 		return nil, err
 	}
-	defer func(file *os.File) {
-		_ = file.Close()
-	}(file)
 
-	d := yaml.NewDecoder(file)
-	if err := d.Decode(&config); err != nil {
-		return nil, err
+	if config.TgToken == "" {
+		config.TgToken = os.Getenv("TG_TOKEN")
+	}
+
+	if config.TgToken == "" {
+		return nil, ErrMissingTgToken
 	}
 
 	return config, nil
+}
+
+var ErrMissingTgToken = &MissingTgTokenError{}
+
+type MissingTgTokenError struct{}
+
+func (e *MissingTgTokenError) Error() string {
+	return "tg_token is missing: not found in config file or environment variable TG_TOKEN"
 }
